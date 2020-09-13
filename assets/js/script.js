eb1fcb6187be4e696e01e7be7285edee7469fa68
André Queiroz <alqlima@gmail.com>
2020-09-13 08:34:11 -0300

    Add elements to the array with push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,39 @@
-let data = [{
-	id: 1,
-	title: "Estudar Html"
-}, {
-	id: 2,
-	title: "Estudar CSS"
-}, {
-	id: 3,
-	title: "Estudar Javascript"
-	
-}, {
-	id: 4,
-	title: "Estudar PHP"
-}];
+let data = [];
 
-data.forEach(task => {
-	let li = document.createElement('li');
+function rendTodo() {
 	
-	li.innerHTML = `
-		<input type="checkbox" id="task-${task.id}">
-		<label for="task-${task.id}">${task.title}</label>
-	`;
-	
-	li.querySelector('input').addEventListener("change", e => {
-		if(e.target.checked) {
-			li.classList.add('complete');
-		} else {
-			li.classList.remove('complete');
-		}
-	})
-	
-	document.querySelector('.todo').append(li);
-});
+	document.querySelector('.todo').innerHTML = '';
+
+	data.forEach(task => {
+		let li = document.createElement('li');
+		
+		li.innerHTML = `
+			<input type="checkbox" id="task-${task.id}">
+			<label for="task-${task.id}">${task.title}</label>
+		`;
+		
+		li.querySelector('input').addEventListener("change", e => {
+			if(e.target.checked) {
+				li.classList.add('complete');
+			} else {
+				li.classList.remove('complete');
+			}
+		})
+		
+		document.querySelector('.todo').append(li);
+	});
+}
 
 document.querySelector('#new-task').addEventListener("keyup", e => {
 	if (e.key === 'Enter') {
 		console.log(e.target.value);
+		
+		data.push({
+			id: data.length + 1,
+			title: e.target.value
+		});
+		e.target.value = '';
+		rendTodo();
 	} 
 });
+rendTodo();
