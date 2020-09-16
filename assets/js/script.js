9b1067ec8b7bee52a1b122cf13e0f3b8832a7e0c
André Queiroz <alqlima@gmail.com>
2020-09-16 18:31:33 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,16 @@ function rendTodo() {
 			}
 		})
 		li.querySelector('button').addEventListener('click', e => {
-			console.log(e.target.parentNode.querySelector('input').id.split('-')[1]);
+			let button = e.target;
+			let li = button.parentNode;
+			let input = li.querySelector('input');
+			let id = input.id;
+			let idArray = id.split('-');
+			let todoId = idArray[1];
+			
+			data = data.filter(task => (task.id != parseInt(todoId)))
+			
+			rendTodo();
 		})
 		document.querySelector('.todo').append(li);
 	});
